a3d4f7d8e5aba73e6ac98d5d59316b70b9f0e101
iswilljr <iswilljr@gmail.com>
2023-06-11 16:49:24 -0500

    feat: configEnvFile fn now only handles the necessary

--- src/index.js
+++ src/index.js
@@ -1,41 +1,27 @@
 import fs from "fs/promises";
-import { resolve } from "path";
+import path from "path";
 import filenamify from "filenamify";
-import { constantCase } from "constant-case";
+import { getConfig, stringifyEnvFileConfig } from "./common.js";
 
-const getConfig = (config, _prefix = "", type = "file", env = "process") => {
-  const isFile = type === "file";
-  const prefix = constantCase(_prefix);
+const VALID_ENV_NAMES = ["process", "import"];
 
-  const newConfig = Object.entries(config).map(([k, value]) => {
-    const name = constantCase(k);
-    const key = `${prefix && !name.includes(prefix) ? `${prefix}_` : ""}${name}`;
-    const config = `${k}: ${env === "process" ? "process.env." : "import.meta.env."}${key}`;
-    return isFile ? `${key}=${value}` : config;
-  });
+export async function configEnvFile(file, options = {}) {
+  const { destination = ".", extension: e, prefix, env = "process" } = options;
 
-  return newConfig.join(isFile ? "\n" : ",\n ") + (isFile ? "\n" : "");
-};
-
-export const configEnvFile = async (file, options = {}) => {
-  const { destination = ".", extension: e, prefix: p, env, exitOnError = false, silent = true } = options;
-
-  try {
-    const prefix = p ? filenamify(p, { replacement: "." }) : undefined;
-    const extension = e ? filenamify(e, { replacement: "." }) || "local" : "local";
-
-    if (!file) throw new Error("Config file is required");
-    if (env && !["process", "import"].includes(env)) throw Error(`received '${env}' expected 'process' or 'import'`);
-
-    const config = JSON.parse(await fs.readFile(file, "utf8"));
-    await fs.writeFile(resolve(destination, `.env.${extension}`), getConfig(config, prefix));
-
-    if (!silent) console.log(`const config = { \n ${getConfig(config, prefix, "config", env)}\n}`);
-  } catch (e) {
-    if (exitOnError) {
-      console.error("error:", e.message);
-      process.exit(1);
-    }
-    throw Error(e.message);
+  if (!file) {
+    throw new Error("Config file is required");
   }
-};
+
+  if (!VALID_ENV_NAMES.includes(env)) {
+    throw Error(`received '${env}' expected 'process' or 'import'`);
+  }
+
+  const extension = (e && filenamify(e, { replacement: "." })) || "local";
+
+  const filePath = path.resolve(destination, `.env.${extension}`);
+
+  const config = await getConfig(file);
+  const envFileString = stringifyEnvFileConfig(config, prefix);
+
+  await fs.writeFile(filePath, envFileString);
+}
